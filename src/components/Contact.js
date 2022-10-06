ec0c8df402237ffa3f6b5982cf7cd42a8d5593c8
first-name second-name <silvia.wachira@student.moringa.com>
2022-10-06 23:21:28 +0300

    add contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,9 +7,14 @@ function Contact() {
     <div className='icons'>
        <h1>Contact Us</h1>
        <div className='social-icons'>
-        <AiFillTwitterCircle />
-        <AiFillInstagram />
-        <FaTiktok />
+          <AiFillTwitterCircle className='social-icon'/>
+          <AiFillInstagram className='social-icon'/>
+          <FaTiktok className='social-icon'/>
+       </div>
+       <div>
+            <p>Email:chakula@gmail.com</p>
+            <p>+254 700000000</p>
+            <p>+254 735009000</p>
        </div>
     </div>
   )
